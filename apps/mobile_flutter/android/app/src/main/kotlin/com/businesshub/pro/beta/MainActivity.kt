9cdd199dcaa56acd1b2dc5106234fd5e1a744a99
MurtazaJ53 <murtaza@example.com>
2026-04-28 00:10:26 +0530

    feat: build launchable flutter mobile beta

---
+++ apps/mobile_flutter/android/app/src/main/kotlin/com/businesshub/pro/beta/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.businesshub.pro.beta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- apps/mobile_flutter/lib/core/firebase/firebase_bootstrap.dart
+++ apps/mobile_flutter/lib/core/firebase/firebase_bootstrap.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_performance/firebase_performance.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
 
 import 'firebase_options.dart';
 
